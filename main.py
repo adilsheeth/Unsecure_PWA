66c48985473ebcb1375a42a7786793f8333a474b
Adil Sheeth <adilsheeth21@gmail.com>
2025-03-28 09:27:09 +1100

    fixes

--- main.py
+++ main.py
@@ -4,20 +4,61 @@ from flask import request
 from flask import redirect
 import user_management as dbHandler
 
+import bleach
+import re 
+
+import hashlib
+
+import html
+
+from flask_wtf.csrf import CSRFProtect
+
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+import hmac
+
+import time
+
+csrf = CSRFProtect()
+
 # Code snippet for logging a message
 # app.logger.critical("message")
 
 app = Flask(__name__)
 
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["200 per day", "50 per hour"]
+)
+
+
 
 @app.route("/success.html", methods=["POST", "GET", "PUT", "PATCH", "DELETE"])
 def addFeedback():
     if request.method == "GET" and request.args.get("url"):
+
         url = request.args.get("url", "")
         return redirect(url, code=302)
     if request.method == "POST":
-        feedback = request.form["feedback"]
-        dbHandler.insertFeedback(feedback)
+        # Input Sanitization + XSS Prevention
+        feedback = bleach.clean(html.escape(request.form["feedback"].strip()))
+        # Input Validation
+        # Side-channel attacks
+        valid_feedback = True
+        for char in feedback:
+            if not re.match(r"^[a-zA-Z0-9 .,!?]*$", char): 
+                valid_feedback = False
+        # Error Handling
+        if not valid_feedback:
+            time.sleep(0.1)
+            return f"An error occurred.", 400
+        try:
+            dbHandler.insertFeedback(feedback)
+        except Exception as e:
+            time.sleep(0.1)
+            return f"An error occurred.", 400 
         dbHandler.listFeedback()
         return render_template("/success.html", state=True, value="Back")
     else:
@@ -34,6 +75,8 @@ def signup():
         username = request.form["username"]
         password = request.form["password"]
         DoB = request.form["dob"]
+        # Hashing
+        password = hashlib.sha256(password.encode()).hexdigest()
         dbHandler.insertUser(username, password, DoB)
         return render_template("/index.html")
     else:
